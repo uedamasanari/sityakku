ed35ca5488c3e665f6ec7915ad50a1be5adbc287
kanapiii <113953054+kanapiii@users.noreply.github.com>
2022-10-31 12:24:07 +0900

    ログイン、新規PHP

--- tameshitxt.php
+++ tameshitxt.php
@@ -5,3 +5,17 @@
     <input type = "pass" name = "pass"><br>
     <input type="submit" value="送信">
 </form>
+
+<h3>ログイン</h3>
+<form action="tameshi.php" method  ="post">
+    mail<input type = "text" name ="mail"><br>
+    pass<input type = "pass" name = "pass"><br>
+    <input type = "submit" value = "login">
+</form>
+
+<h3>新規登録</h3>
+<form action="tameshi.php" method = "post">
+    mail<input type="text" name="mail1"><br>
+    pass<input type="pass" name = "pass1"><br>
+    <input type = "submit" value="新規登録">
+</form>
