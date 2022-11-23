3bbc3f0385eac3cc29e397c563b2b83b8bc6ffc8
明石凜太 <jeffrey.bezos@icloud.com>
2022-11-23 15:19:21 +0900

    mobile edit
    （’’~pーp’』）

---
+++ mobile-file/tabmenu.js
@@ -0,0 +1,30 @@
+$('.tabmenu-wrap .tab-nav').find('a').on('click', function(e) {
+   var $this = $(this);
+	var $all_tab_nav = $this.parents('.tab-nav').find('.nav');
+	var $tab_contents = $this.parents('.tabmenu-wrap').find('.con-box');
+	var id = $this.attr('href');
+
+	e.preventDefault();
+	$all_tab_nav.removeClass('on');
+	$this.parent().addClass('on');
+	$tab_contents.hide();
+	$(id).show();
+});
+
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#modal-close , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+  });
+
+  $(function () {
+    $('#openSyousai').click(function(){
+        $('#SyousaiArea').fadeIn();
+    });
+    $('#Syousai-close , #SyousaiBg').click(function(){
+      $('#SyousaiArea').fadeOut();
+    });
+  });
