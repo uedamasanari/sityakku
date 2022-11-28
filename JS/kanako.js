fc9d7ffcfc6fd714e841abd77d6c75960d396aae
kanapiii <113953054+kanapiii@users.noreply.github.com>
2022-11-28 15:02:38 +0900

    JS購入確定変更

--- JS/kanako.js
+++ JS/kanako.js
@@ -49,12 +49,24 @@ ugoku.addEventListener('click',function(){
 
     let c = sessionStorage.getItem('id');
     $.ajax({
-        url: `PHP/itoyama.php/?cart=true&id=${c}$timestamp=${new Date().getTime()}`
+        url: `PHP/itoyama.php/?profile=true&user_id=${c}&timestamp=${new Date().getTime()}`
     })
     .success(function(mo) {
         console.log(mo);
 
-        console.log('購入画面へ進める');
+        //console.log('購入画面へ進める');
+        //支払い方法選択表示
+        let select = document.getElementById("sel1");
+        if(mo[0].user_buy == "銀行振込"){
+            select.options[1].selected = true;
+        }else if(mo[0].user_buy == "コンビニ払い"){
+            select.options[2].selected = true;
+        }else if(mo[0].user_buy == "クレジットカード"){
+            select.options[3].selected = true;
+        }else{
+            select.options[4].selected = true;
+        }
+        
 
         
     }).error(function(XMLHttpRequest, textStatus, errorThrown) {
@@ -62,5 +74,79 @@ ugoku.addEventListener('click',function(){
         console.log("textStatus     : " + textStatus);
         console.log("errorThrown    : " + errorThrown.message);
     });
+
+});
+
+//購入確定ボタンクリック時の動き
+let kaku = document.getElementById('kakunin');
+kaku.addEventListener('click',function(){
+
+        let id = sessionStorage.getItem('id');
+        let siha = document.getElementById('sel1').value;
+        let yuubin = document.getElementById('yuubin').value;
+        let ken = document.getElementById('ken').value;
+        let sityou = document.getElementById('sityou').value;
+        let banti = document.getElementById('banti').value;
+        let heyaban = document.getElementById('heyaban').value;
+        let tosi = document.getElementById('bday-year').value;
+        let tuki = document.getElementById('bday-month').value;
+        let day = document.getElementById('bday-day').value;
+
+        //支払い方法などのデータをlocalstorageで保存
+        if(window.localStorage){
+            let e = sessionStorage.getItem('id');
+            let data = {
+                id:e,
+                siha:document.getElementById('sel1').value,
+            };
+            let json = JSON.stringify(data,undefined,1);
+            localStorage.setItem('key',json);
+        }
+        let data1 = localStorage.getItem('key');
+        data1 = JSON.parse(data1);
+        console.log(data1);
+        let kakusiha = document.getElementById('kaku_siha');
+        //kakusiha.innerHTML = data1.
+
+        let d = sessionStorage.getItem('id');
+        $.ajax({
+            url: `PHP/itoyama.php/?cart=true&id=${d}$timestamp=${new Date().getTime()}`
+        })
+
+        .success(function(res) {
+            console.log(res);
+            let list = document.getElementsByClassName('swiper-slide modalInSlider');
+            for(let i = 0; i<res.length;i++){
+                let ele = document.createElement('div');
+                ele.className = 'list-all';
+                list.appendChild(ele);
+                ele = document.createElement('ol');
+                ele.className = 'sample1';
+                list.appendChild(ele);
+                ele = document.createElement('li');
+                ele.className = 'li';
+                let tag = document.getElementsByClassName('sample1');
+                tag.appendChild(ele);
+                ele = document.createElement('img');
+                ele.src = "";
+                ele.alt = "";
+                ele.className = 'i-img';
+                tag = document.getElementsByClassName('li');
+                tag.appendChild(ele);
+                ele = document.createElement('div');
+                ele.className = 'ko-gona';
+                ele.textContent = res[i].item_name;
+                tag.appendChild(ele);
+                ele = document.createElement('div');
+                ele.className = 'ko-dol';
+                ele.textContent = res[i].item_money;
+            }
+
+            
+        }).error(function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+            console.log("textStatus     : " + textStatus);
+            console.log("errorThrown    : " + errorThrown.message);
+        });
 
 });
