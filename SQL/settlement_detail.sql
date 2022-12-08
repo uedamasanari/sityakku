b483f3166dc951aca735d5017f5dd9e675281079
doraemon123453@gmail.com <doraemon123453@gmail.com>
2022-12-09 00:16:44 +0900

    ああ

--- SQL/settlement_detail.sql
+++ SQL/settlement_detail.sql
@@ -1,6 +1,12 @@
 CREATE TABLE settlement_detail(
     settlement_detail_id      INT      AUTO_INCREMENT,
     settlement_id             INT      NOT NULL,
+<<<<<<< Updated upstream
     item_id                   INT      NOT NULL,
     PRIMARY KEY (settlement_detail_id)
+=======
+    item_name                   INT      NOT NULL,
+    PRIMARY KEY (settlement_detail_id,settlement_id),
+    FOREIGN KEY (settlement_id) REFERENCES settlement(settlement_id),
+>>>>>>> Stashed changes
 );
